2e10785207ca014559912adeb280bc8e3bc6d59e
rudyluis <rudy.manzaneda@gmail.com>
2025-05-22 23:08:57 -0400

    2205020205

--- app.py
+++ app.py
@@ -235,6 +235,6 @@ def actualizar_videojuego(id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    ##port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto dinámicamente
-    ##app.run(host='0.0.0.0', port=port)
+    ##app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto dinámicamente
+    app.run(host='0.0.0.0', port=port)
